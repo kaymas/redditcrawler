d3211c2a1a4188caa39254483aeb854b05d7f150
kaymas <samyakahuja24@hotmail.com>
2018-02-13 17:53:42 +0530

    added reddit api and cards

--- index.js
+++ index.js
@@ -17,6 +17,32 @@ searchForm.addEventListener('submit', e => {
 
     //search reddit
     reddit.search(searchTerm, searchLimit, sortBy)
+        .then(results => {
+            let output = '<div class="card-columns">'
+            
+            results.forEach(post => {
+                let image = post.preview ? post.preview.images[0].source.url : 'https://cdn.comparitech.com/wp-content/uploads/2017/08/reddit-1.jpg'
+                let permalink = 'https://www.reddit.com/' + post.permalink
+                output += `
+                <div class="card"> 
+                    <img class="card-img-top" src="${image}" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">${post.title}</h5>
+                        <p class="card-text">${truncateText(post.selftext, 100)}</p>
+                        <a href="${post.url}" target="_blank" class="btn btn-primary">Read more</a>
+                        <hr>
+                        <span class="badge badge-secondary">Subreddit : ${post.subreddit}</span>
+                        <span class="badge badge-secondary"><a href="${permalink}" style="color: white"target="_blank">COMMENTS</a></span>
+                        <span class="badge badge-dark">Score : ${post.score}</span>
+                    </div>
+                </div>        
+                `
+                
+            })
+            output += '</div>'
+            document.getElementById('results').innerHTML = output
+
+        })
 
     e.preventDefault()
 })
@@ -33,4 +59,10 @@ function showMessage(message, className){
     searchContainer.insertBefore(div, search)
 
     setTimeout(() => document.querySelector('.alert').remove(), 3000)
+}
+
+function truncateText(text, limit){
+    const shortened = text.indexOf(' ', limit)
+    if(shortened == -1) return text
+    return text.substring(0,shortened)
 }
